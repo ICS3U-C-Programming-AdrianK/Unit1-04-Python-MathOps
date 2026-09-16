67f1932f34be2d5f5d141f890675667a30bca6c0
Adrian kouakam <adrian.kouakam-guemiebing@stu.ocsb.ca>
2026-09-16 03:01:48 +0000

    first commit basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By:Adrian student
+# Date: sep 15th, 2026
+# This program does basic math
+#
+def main():
+    print("7-3={}".format(7 - 3))
+    print("5+4={}".format(5 + 4))
+    print("8*8={}".format(8 * 8))
+    print("9/2={}".format(9 / 2))
+    print("2+5*3={}".format(2 + 5 * 3))
+    print("2^3={}".format(2 ^ 3))
+    print("2**3={}".format(2**3))
+    print("36**0.5={}".format(36**0.5))
+
+
+if __name__ == "__main__":
+    main()
